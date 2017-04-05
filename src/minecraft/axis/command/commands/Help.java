d92666935de1d81abc6ecd1342bb981eeadad74b
BuildTools <unconfigured@null.spigotmc.org>
2017-04-05 14:11:52 +0900

    一応のhelp追加

---
+++ src/minecraft/axis/command/commands/Help.java
@@ -0,0 +1,17 @@
+package axis.command.commands;
+
+import axis.Axis;
+import axis.command.Command;
+import axis.module.Module;
+import axis.util.Logger;
+
+public final class Help extends Command {
+	public Help() {
+		super("Help", "<>", new String[] { "he" });
+	}
+
+	public void run(String message) {
+		Logger.logChat(
+				"Commands list: add, bind, del, help, toggle, vclipx, vclipy, vclipz, aimassist, autopot, criticals, killaura, regen, velocity, setting, civbreak, leastcivbreak, fly, jesus, speed, step, autoarmor, esp, storageesp, worldtimer");
+	}
+}
